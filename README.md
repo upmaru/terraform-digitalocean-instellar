fa921d0cc3805b3e5ba974ad04fbe6ade592b144
Zack Siri <me@zacksiri.dev>
2022-11-21 16:29:02 +0700

    Add diagram

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Terraform for Instellar
 
 This repository is used to store all terraform files for bootstrapping clusters to be used with instellar.app
+
+This is the setup that is recommended for hosting production apps. It sets up the following components
+
++ Bastion 
++ Compute Nodes
++ VPC
++ Firewall
++ Database
+
+## Design
+
+![design](/assets/design.jpeg)
--- main.tf
+++ main.tf
@@ -2,16 +2,56 @@ provider "digitalocean" {
   token = var.do_token
 }
 
-resource "digitalocean_droplet" "this" {
-  count    = var.cluster_size
-  image    = var.image
-  name     = "${var.project_name}-0${count.index}"
-  region   = var.region
-  size     = var.size
-  ssh_keys = var.ssh_keys
+resource "digitalocean_vpc" "nodes_vpc" {
+  name        = "${var.cluster_name}-instellar-vpc"
+  description = "VPC used for instellar.app nodes"
+  region      = var.region
+  ip_range    = var.vpc_ip_range
 }
 
-resource "digitalocean_project" "this" {
-  name = var.project_name
-  resources = digitalocean_droplet.this[*].urn
+resource "digitalocean_droplet" "bastion" {
+  image  = var.image
+  name   = "${var.cluster_name}-bastion"
+  region = var.region
+  size   = var.bastion_size
+}
+
+resource "digitalocean_droplet" "nodes" {
+  count    = var.cluster_size
+  image    = var.image
+  name     = "${var.cluster_name}.0${count.index + 1}"
+  region   = var.region
+  size     = var.node_size
+  ssh_keys = var.ssh_keys
+  vpc_uuid = digitalocean_vpc.nodes_vpc.id
+}
+
+resource "digitalocean_firewall" "nodes_firewall" {
+  name = "${var.cluster_name}-instellar-nodes"
+
+  droplet_ids = digitalocean_droplet.nodes[*].id
+
+  # SSH is only open to bastion node
+  inbound_rule {
+    protocol           = "tcp"
+    port_range         = "22"
+    source_droplet_ids = [digitalocean_droplet.bastion.id]
+  }
+  
+  # Enable instellar to communicate with the nodes
+  inbound_rule {
+    protocol = "tcp"
+    port_range = "8443"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_firewall" "bastion_firewall" {
+  
+}
+
+resource "digitalocean_project" "project" {
+  name        = var.cluster_name
+  environment = var.environment
+  resources   = digitalocean_droplet.nodes[*].urn
 }
