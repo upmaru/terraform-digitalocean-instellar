8669f838e3acea0c80c661bb5bd76414c535b1d0
Zack Siri <me@zacksiri.dev>
2022-11-22 14:57:19 +0700

    update firewall rules

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ resource "digitalocean_tag" "db_access" {
   name = "${var.cluster_name}-db-access"
 }
 
+resource "digitalocean_tag" "instellar_node" {
+  name = "${var.cluster_name}-node"
+}
+
+resource "digitalocean_tag" "instellar_bastion" {
+  name = "${var.cluster_name}-bastion"
+}
+
 resource "digitalocean_vpc" "cluster_vpc" {
   name        = "${var.cluster_name}-instellar-vpc"
   description = "VPC used for https://instellar.app nodes"
@@ -20,24 +28,24 @@ resource "digitalocean_droplet" "bastion" {
   size     = var.bastion_size
   ssh_keys = var.ssh_keys
   vpc_uuid = digitalocean_vpc.cluster_vpc.id
-  tags     = [digitalocean_tag.db_access.id]
+  tags     = [digitalocean_tag.db_access.id, digitalocean_tag.instellar_bastion.id]
 
 }
 
 resource "digitalocean_droplet" "nodes" {
   count    = var.cluster_size
   image    = var.image
-  name     = "${var.cluster_name}.0${count.index + 1}"
+  name     = "${var.cluster_name}-node.0${count.index + 1}"
   region   = var.region
   size     = var.node_size
   vpc_uuid = digitalocean_vpc.cluster_vpc.id
-  tags     = [digitalocean_tag.db_access.id]
+  tags     = [digitalocean_tag.db_access.id, digitalocean_tag.instellar_node.id]
 }
 
 resource "digitalocean_firewall" "nodes_firewall" {
   name = "${var.cluster_name}-instellar-nodes"
 
-  droplet_ids = digitalocean_droplet.nodes[*].id
+  tags = [digitalocean_tag.instellar_node.id]
 
   # SSH is only open to bastion node
   inbound_rule {
@@ -51,6 +59,19 @@ resource "digitalocean_firewall" "nodes_firewall" {
     protocol         = "tcp"
     port_range       = "8443"
     source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+  
+  # Enable full cross-node communication
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "10000-65535"
+    source_tags      = [digitalocean_tag.instellar_node.id]
+  }
+  
+  inbound_rule {
+    protocol         = "udp"
+    port_range       = "10000-65535"
+    source_tags      = [digitalocean_tag.instellar_node.id]
   }
 
   # Enable all outbound traffic
