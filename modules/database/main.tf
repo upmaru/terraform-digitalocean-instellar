2a107e00e50a87446fd5293421c502f29791f480
Zack Siri <me@zacksiri.dev>
2022-11-22 13:22:56 +0700

    Add size

--- modules/database/main.tf
+++ modules/database/main.tf
@@ -8,6 +8,7 @@ resource "digitalocean_database_cluster" "database" {
   engine               = var.engine
   version              = var.engine_version
   region               = var.region
+  size                 = var.db_cluster_size
   node_count           = var.db_node_count
   private_network_uuid = var.cluster_vpc_id
 }
